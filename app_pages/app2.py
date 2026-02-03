86397947940fe359da4f85bf385cd14a3e3c32c1
Omisa Kamath <omisa.59kamath@gmail.com>
2026-02-03 11:33:08 +0100

    created multiple pages one to take query and one recommender

---
+++ app_pages/app2.py
@@ -0,0 +1,11 @@
+import streamlit as streamlit
+import streamlit.components.v1 as components
+
+st.title("Avant Garde Designer Recommender")
+
+
+
+
+
+
+# two containers have been created and part of the controller logic is done. 
--- app_pages/recommendation.py
+++ app_pages/recommendation.py
@@ -1,8 +1,6 @@
 import streamlit as streamlit
 import streamlit.components.v1 as components
 
-st.title("Avant Garde Designer Recommender")
-
 components.html(
     """
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -11,7 +9,8 @@ components.html(
         <div class="container text-center">
             <div class="row">
                 <div class="col">
-                Text Col
+                    <div class="vertical-text">
+                        <h1>
                 </div>
                 <div class="col">
                 picture col
@@ -22,5 +21,3 @@ components.html(
     """,
     height=600
 )
-
-# two containers have been created and part of the controller logic is done. 
